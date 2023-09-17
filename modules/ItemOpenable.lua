0215cb306566f3eb42f8cd0d534d1e254bce6ce3
VicarInATutu <spamqo@gmail.com>
2023-09-17 18:56:33 -0500

    * Bug fix and simplify ItemOpenable marking of items * Update TOC to latest client version

--- modules/ItemOpenable.lua
+++ modules/ItemOpenable.lua
@@ -19,7 +19,6 @@ mod.uiName = L['Item openable']
 mod.uiDesc = L['Mark an item as openable.']
 
 local texts = {}
-local updateCache = {}
 
 function mod:OnEnable()
 	self:RegisterMessage('AdiBags_UpdateButton', 'UpdateButton')
@@ -27,78 +26,69 @@ function mod:OnEnable()
 end
 
 function mod:OnDisable()
-	wipe(updateCache)
 	for _, text in pairs(texts) do
 		text:Hide()
 	end
 end
 
-local function CreateText(button)
-	local text = button:CreateFontString(nil, "OVERLAY", "NumberFontNormal")
-	text:SetPoint("TOPLEFT", button, 3, -1)
-	text:Hide()
-	texts[button] = text
-	return text
-end
-
 
 function mod:UpdateButton(event, button)
 	local text = texts[button]
-	local link = button:GetItemLink()
-
-	if link then
-		local _, _, quality, _, reqLevel, _, _, _, loc = GetItemInfo(link)
-		local item = Item:CreateFromBagAndSlot(button.bag, button.slot)
-		local openText = isOpenable(button)
-		if openText then
-			if not text then
-				text = CreateText(button)
-			end
-			text:SetText(openText)
-			text:SetTextColor(.1,1,.1)
-			text:Show()
-		else
-			if text then
-				text:Hide()
-			end
-		end
+	if not text then
+		text = button:CreateFontString(nil, "OVERLAY", "NumberFontNormal")
+		text:SetPoint("TOPLEFT", button, 3, -1)
+		texts[button] = text
 	end
+
+	text:Hide()
+	if isOpenable(button) then
+		text:SetText("Open")
+		text:SetTextColor(.1,1,.1)
+		text:Show()
+	end
+
+
 end
 
 
 function isOpenable(button)
-	local tooltip = CreateFrame("Gametooltip")
-	tooltip:SetOwner(UIParent,"ANCHOR_NONE")
-	tooltip:ClearLines()
-
 	local isRetail = C_TooltipInfo
 
 	if isRetail then
-		tooltip = C_TooltipInfo.GetBagItem(button.bag, button.slot)
+		local tooltip = C_TooltipInfo.GetBagItem(button.bag, button.slot)
+		if not tooltip then
+			return false
+		end
     for i, line in ipairs(tooltip.lines) do
 			local ttText = line.leftText
 			if ttText and (string.find(ttText, OPENABLE_CONTAINER) or string.find(ttText, OPENABLE_USEOPEN) or string.find(ttText, OPENABLE_RIGHTCLICK)) then
-				return "Open"
+				return true
 			end
     end
 	else
-	if button.bag == BANK_CONTAINER then
-		tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(button.slot, nil))
-	else
-		tooltip:SetBagItem(button.bag, button.slot)
-	end
-  local regions = {GameTooltip:GetRegions()}
-  for i, r in ipairs(regions) do
-    if r:IsObjectType("FontString") then
-      local ttText = r:GetText()
+		GameTooltip:SetOwner(UIParent,"ANCHOR_NONE")
+		GameTooltip:ClearLines()
+		if button.bag == BANK_CONTAINER then
+			GameTooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(button.slot, nil))
+		else
+			GameTooltip:SetBagItem(button.bag, button.slot)
+		end
+
+		local regions = {GameTooltip:GetRegions()}
+		for i, r in ipairs(regions) do
+			if r:IsObjectType("FontString") then
+				local ttText = r:GetText()
 				if ttText and (string.find(ttText, OPENABLE_CONTAINER) or string.find(ttText, OPENABLE_USEOPEN) or string.find(ttText, OPENABLE_RIGHTCLICK)) then
-					return "Open"
+					return true
 				end
 			end
 		end
+
+		GameTooltip:Hide()
+
 	end
 
-	return nil
+	return false
 end
 
 
